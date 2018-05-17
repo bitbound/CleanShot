c7abc87b7b9cd35169bfe8dd341de35b933bb1b9
Jared Goodwin <translucency@outlook.com>
2018-05-16 17:25:18 -0700

    Performance increase in GIF recorder.

--- CleanShot/Classes/GIFRecorder.cs
+++ CleanShot/Classes/GIFRecorder.cs
@@ -19,33 +19,23 @@ namespace CleanShot.Classes
     public static class GIFRecorder
     {
         public static RecordingState State { get; set; } = RecordingState.Stopped;
-        private static Bitmap previousBitmap { get; set; }
-        private static string tempPath { get; set; } = Path.Combine(Path.GetTempPath(), "CleanShot");
-        public static void Record(Rect Region)
+        private static Bitmap PreviousBitmap { get; set; }
+        private static GifBitmapEncoder GifEncoder { get; set; }
+        private static Bitmap LastFrame { get; set; }
+        public static async void Record(Rect Region)
         {
             if (State == RecordingState.Recording)
             {
                 return;
             }
-            if (Directory.Exists(tempPath))
-            {
-                try
-                {
-                    Directory.Delete(tempPath, true);
-                }
-                catch
-                {
-                    System.Windows.MessageBox.Show("Unable to remove temp files.  Try closing and reopening CleanShot.", "File Delete Failure", MessageBoxButton.OK, MessageBoxImage.Error);
-                }
-            }
-            var di = Directory.CreateDirectory(tempPath);
-            State = RecordingState.Recording;
-            previousBitmap = new Bitmap((int)Region.Width, (int)Region.Height);
-            Task.Run(async () =>
-            {
-                var ticks = 0;
-                var captureStart = DateTime.Now;
 
+            GifEncoder = new GifBitmapEncoder();
+
+            State = RecordingState.Recording;
+            PreviousBitmap = new Bitmap((int)Region.Width, (int)Region.Height);
+            var captureStart = DateTime.Now;
+            try
+            {
                 while (State != RecordingState.Stopped)
                 {
                     if (State == RecordingState.Paused)
@@ -59,64 +49,42 @@ namespace CleanShot.Classes
                     {
                         using (var ms = new MemoryStream())
                         {
-                            screenshot.Save(Path.Combine(di.FullName, ticks.ToString() + ".png"), ImageFormat.Png);
+                            if (GifEncoder.Frames.Count == 0)
+                            {
+                                screenshot.Save(ms, ImageFormat.Png);
+                            }
+                            else
+                            {
+                                ImageDiff.GetDifference(screenshot, LastFrame).Save(ms, ImageFormat.Png);
+                            }
+                           
+                            GifEncoder.Frames.Add(BitmapFrame.Create(ms, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.OnLoad));
                         }
+                      
                         var captureTime = (DateTime.Now - captureStart).TotalMilliseconds;
-                        var delayTime = Math.Max(0, 100 - (int)captureTime);
+                        var delayTime = Math.Max(1, 100 - (int)captureTime);
+                        LastFrame = (Bitmap)screenshot.Clone();
                         await Task.Delay(delayTime);
-                        ticks++;
                     }
                 }
-            });
+            }
+            catch (OutOfMemoryException)
+            {
+                System.Windows.MessageBox.Show("The application has run out of available memory.  Try creating a shorter GIF.", "Out of Memory", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
         }
         public static void Encode()
         {
-            if (!Directory.Exists(tempPath) || Directory.GetFiles(tempPath).Length == 0)
-            {
-                return;
-            }
             try
             {
-                var gifEncoder = new GifBitmapEncoder();
-
-                for (var i = 0; i < Directory.GetFiles(tempPath).Length; i++)
-                {
-                    if (i == 0)
-                    {
-                        using (var fs = new FileStream(Path.Combine(tempPath, i.ToString() + ".png"), FileMode.Open, FileAccess.Read))
-                        {
-                            gifEncoder.Frames.Add(BitmapFrame.Create(fs, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.OnLoad));
-                        }
-                    }
-                    else
-                    {
-                        using (var bitmap1 = (Bitmap)Bitmap.FromFile(Path.Combine(tempPath, i.ToString() + ".png")))
-                        {
-                            using (var bitmap2 = (Bitmap)Bitmap.FromFile(Path.Combine(tempPath, (i - 1).ToString() + ".png")))
-                            {
-                                var diff = ImageDiff.GetDifference(bitmap1, bitmap2);
-                                using (var ms = new MemoryStream())
-                                {
-                                    try
-                                    {
-                                        diff.Save(ms, ImageFormat.Png);
-                                    }
-                                    catch
-                                    {
-                                        bitmap1.Save(ms, ImageFormat.Png);
-                                    }
-                                    gifEncoder.Frames.Add(BitmapFrame.Create(ms, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.OnLoad));
-                                }
-                            }
-                        }
-                    }
-                }
-
                 var di = Directory.CreateDirectory(Settings.Current.SaveFolder);
                 var saveFile = Path.Combine(di.FullName, "CleanShot_" + DateTime.Now.ToString("yyyy-MM-dd_HH.mm.ss.ff") + ".gif");
+
+          
                 using (var ms = new MemoryStream())
                 {
-                    gifEncoder.Save(ms);
+        
+                    GifEncoder.Save(ms);
                     var fileBytes = ms.ToArray();
                     var applicationExtension = new byte[] { 33, 255, 11, 78, 69, 84, 83, 67, 65, 80, 69, 50, 46, 48, 3, 1, 0, 0, 0 };
                     var newBytes = new List<byte>();
@@ -124,27 +92,15 @@ namespace CleanShot.Classes
                     newBytes.AddRange(applicationExtension);
                     newBytes.AddRange(fileBytes.Skip(13));
                     File.WriteAllBytes(saveFile, newBytes.ToArray());
-                    
                 }
-                gifEncoder.Frames.Clear();
+                GifEncoder.Frames.Clear();
                 GIFViewer.Create(saveFile);
-               
+
             }
             catch (OutOfMemoryException)
             {
                 System.Windows.MessageBox.Show("The application has run out of available memory.  Try creating a shorter GIF.", "Out of Memory", MessageBoxButton.OK, MessageBoxImage.Error);
             }
-
-
-            try
-            {
-                Directory.Delete(tempPath, true);
-            }
-            catch {
-                System.Windows.MessageBox.Show("Unable to remove temp files.  Try closing and reopening CleanShot.", "File Delete Failure", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-
-            
         }
         public enum RecordingState
         {
